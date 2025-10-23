ff1bb345ee11d77cea062e105dc892223229293a
yzhuye <yzhu@uninorte.edu.co>
2025-10-23 03:08:36 -0500

    fix structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AuthHttpModule } from './auth/interface/http/auth.module';
+import { UsersModule } from './auth/interface/http/users/users.module';
 import { ChallengesHttpModule } from './challenge/interface/http/challenges.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-      AuthHttpModule,
-      ChallengesHttpModule,
+    UsersModule,
+    ChallengesHttpModule,
   ],
 })
 export class AppModule {}
